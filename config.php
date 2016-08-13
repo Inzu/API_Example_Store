14b56801651116ae890098fe16feb5d0fd6be812
Inzudotnet <Laptop@lewis-macbook.local>
2016-08-13 13:33:36 +0100

    update

--- config.php
+++ config.php
@@ -5,12 +5,6 @@ $api_key = "02c4e87c154b34b5b06be242a36d54dd";
 
 $api_base = "http://api.inzu.net/1.4";
 
-$pay_url = 'https://payments.inzu.net/'; //Current Inzu payment URL
-$pay_callback = 'http://mywebsite.com/complete'; //Your website order completion page
-
-$loc = "us"; //Set the lowercase two character code for currency location (United States)
-$currency = "&#36;"; //The currency symbol (Dollars)
-
 
 //Check API connection can be established or print error
 $json = file_get_contents("{$api_base}/general/account_live?api_key={$api_key}");
@@ -22,6 +16,15 @@ if($data->live_status != "true"){
 	exit();
 	
 }
+
+
+//E-commerce
+
+$pay_url = 'https://payments.inzu.net/'; //Current Inzu payment URL
+$pay_callback = 'http://mywebsite.com/complete'; //Your website order completion page
+
+$loc = "us"; //Set the lowercase two character code for currency location (United States)
+$currency = "&#36;"; //The currency symbol (Dollars)
 
 
 ?>
